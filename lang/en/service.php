d4a6e477b3660532d3fcecdbe87420cb1b67496e
José Solorzano <solorzano202009@gmail.com>
2019-07-03 18:08:46 -0400

    Traducción de la página de servicios (service.php).

---
+++ lang/en/service.php
@@ -0,0 +1,59 @@
+<?php
+
+// Textos de la página de servicios
+
+$lang['service']['title'] = "Service";
+
+$lang['service']['title_header'] = "We believe that, if done well, technology simplifies. <br> But the simple can be complicated. That's why we are here.";
+
+$lang['service']['subtitle_header'] = "We provide technological solutions prepared by a team of creatives and engineers, <br> who make the work easier for our clients and their clients.";
+
+$lang['service']['group_title'] = "Consultancy";
+
+$lang['service']['group_text'] = "In the world there are thousands of technological tools and services with low implementation and / or development costs, which improve the productivity of organizations. We guide you through the twists and turns that make up your digital landscape; for the selection, adaptation, development and implementation of technological solutions. Our services include:";
+
+$lang['service']['group_column1_title'] = "Tecnology";
+
+$lang['service']['group_column1_text1'] = "Design and development of websites.";
+
+$lang['service']['group_column1_text2'] = "Software Architecture";
+
+$lang['service']['group_column1_text3'] = "Software development";
+
+$lang['service']['group_column1_text4'] = "Electronic commerce and online orders.";
+
+$lang['service']['group_column1_text5'] = "Integration of API systems and services";
+
+$lang['service']['group_column2_text1'] = "Platform evaluation.";
+
+$lang['service']['group_column2_text2'] = "Software tests.";
+
+$lang['service']['group_column3_title'] = "Design";
+
+$lang['service']['group_column3_text1'] = "Design of corporate images.";
+
+$lang['service']['group_column3_text2'] = "Campaign design";
+
+$lang['service']['group_column3_text3'] = "Digital brand management";
+
+$lang['service']['group_column3_text4'] = "Development of the user interface";
+
+$lang['service']['group_column3_text5'] = "Product design";
+
+$lang['service']['group_column4_title'] = "Digital marketing";
+
+$lang['service']['group_column4_text1'] = "Content strategy";
+
+$lang['service']['group_column4_text2'] = "Campaigns and execution strategy.";
+
+$lang['service']['group_column4_text3'] = "Community Manager";
+
+$lang['service']['wrap1_text'] = "We encourage the use of agile methodologies, with fast deliveries, where our team integrates with your organization to understand the real needs, forming an association that combines efforts to create technological solutions that streamline the work of your organization, automate routine tasks and bring welfare to your team and your customers.";
+
+$lang['service']['icon_box1_text'] = "Greater adoption";
+
+$lang['service']['icon_box2_text'] = "More active participation of users";
+
+$lang['service']['icon_box3_text'] = "Customer satisfaction";
+
+?>
---
+++ lang/es/service.php
@@ -0,0 +1,59 @@
+<?php
+
+// Textos de la página de servicios
+
+$lang['service']['title'] = "Servicio";
+
+$lang['service']['title_header'] = "Creemos que, si se hace bien, la tecnología simplifica. <br> Pero lo simple puede ser complicado. Por eso estamos aquí.";
+
+$lang['service']['subtitle_header'] = "Proporcionamos soluciones tecnológicas preparadas por un equipo de creativos e ingenieros, <br> que hacen el trabajo más fácil para nuestros clientes y sus clientes.";
+
+$lang['service']['group_title'] = "Consultoria";
+
+$lang['service']['group_text'] = "En el mundo existen miles de herramientas tecnologías y servicios tecnológicos con bajos costos de implementación y/o desarrollo, que mejoran la productividad de las organizaciones. Te guiamos a través de los giros y vueltas que conforma tu panorama digital; para la la selección, adaptación, desarrollo e implementación de soluciones tecnológicas. Nuestros servicios incluyen:";
+
+$lang['service']['group_column1_title'] = "Tecnología";
+
+$lang['service']['group_column1_text1'] = "Diseño y desarrollo de sitios web.";
+
+$lang['service']['group_column1_text2'] = "Arquitectura de software";
+
+$lang['service']['group_column1_text3'] = "Desarrollo de software";
+
+$lang['service']['group_column1_text4'] = "Comercio electrónico y pedidos en línea.";
+
+$lang['service']['group_column1_text5'] = "Integración de sistemas y servicios API";
+
+$lang['service']['group_column2_text1'] = "Evaluación de plataformas.";
+
+$lang['service']['group_column2_text2'] = "Pruebas de software.";
+
+$lang['service']['group_column3_title'] = "Diseño";
+
+$lang['service']['group_column3_text1'] = "Diseño de imágenes corporativas.";
+
+$lang['service']['group_column3_text2'] = "Diseño de campaña";
+
+$lang['service']['group_column3_text3'] = "Gestión de marca digital";
+
+$lang['service']['group_column3_text4'] = "Desarrollo de la interfaz de usuario";
+
+$lang['service']['group_column3_text5'] = "Diseño de producto";
+
+$lang['service']['group_column4_title'] = "Marketing Digital";
+
+$lang['service']['group_column4_text1'] = "Estrategia de contenido";
+
+$lang['service']['group_column4_text2'] = "Campañas y estrategia de ejecución.";
+
+$lang['service']['group_column4_text3'] = "Community Manager";
+
+$lang['service']['wrap1_text'] = "Fomentamos el uso metodologías ágiles, con entregas rápidas, donde nuestro equipo se integra con tu organización para comprender las necesidades reales, formando una asociación que auna esfuerzos para crear soluciones tecnológicas que agilizan el trabajo de tu organización, automatizan tareas rutinarias y aportan bienestar a tu equipo y tus clientes.";
+
+$lang['service']['icon_box1_text'] = "Mayor adopción";
+
+$lang['service']['icon_box2_text'] = "Participación más activa de los usuarios";
+
+$lang['service']['icon_box3_text'] = "Satisfacción de los clientes";
+
+?>
