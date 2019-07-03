e290bca7a586e4da9362c7aabea87756e8ab9f31
José Solorzano <solorzano202009@gmail.com>
2019-07-03 11:26:30 -0400

    Validación de inclusión del menú y el pie de página de forma específica.

--- company.php
+++ company.php
@@ -65,7 +65,7 @@ include "config.php";
                                     <nav id="menu">
                                     <!-- Inclusión del menú -->
 									<?php 
-									if($config['config']['menu']['active'] == 1){
+									if($config['config']['menu']['company']['active'] == 1){
 									  
 										include "menu.php";
 
@@ -301,93 +301,16 @@ include "config.php";
             </div>
         </div>
         
-        <input id="lang" type="hidden" value="<?php echo $idiom; ?>">
+        <input id="idiom" type="hidden" value="<?php echo $idiom; ?>">
         
-        <footer id="Footer" class="clearfix">
-            <div class="widgets_wrapper" style="padding:120px 0 80px">
-                <div class="container">
-                    <div style="width: 98% !important;margin: 0 1% 0px !important;">
-                        <aside class="widget_text widget widget_custom_html">
-                            <div class="textwidget custom-html-widget">
-                                <h4>Transformamos la forma de trabajar de
-                                    <br> empresas en todo el mundo.</h4>
-                                <hr class="no_line" style="margin:0 auto 40px">
-                                <div class="image_frame image_item no_link scale-with-grid alignnone no_border">
-                                    <div class="image_wrapper">
-                                        <img class="scale-with-grid" src="images/robotics-home-footerline.png">
-                                    </div>
-                                </div>
-                                <hr class="no_line" style="margin:0 auto 10px">
-                                <p>
-                                    <a href="https://www.google.com/maps/place/eittech/@10.2483845,-67.5928161,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x7eeaa7b9c7a0a0ea!8m2!3d10.2483845!4d-67.5906274?hl" target="_blank"><i class="icon-gplus"></i></a>
-									<a href="https://www.linkedin.com/company/eittech" target="_blank"><i class="icon-linkedin"></i></a>
-						
-                                </p>
-                            </div>
-                        </aside>
-                    </div>
-					<!--
-					<div class="column one-fourth">
-                        <aside class="widget_text widget widget_custom_html">
-                            <a href="http://develop.imago.web.ve/eittech/develop1/html/content/robotics/products.html">
-                                    <h4>Productos</h4>
-                            </a>
-                            
-                            <div class="textwidget custom-html-widget">
-                                <ul>
-                                  <li>
-                                    <i class="themecolor icon-right-open-mini"></i>BIM
-                                 </li>
-                                     <li>
-                                        <i class="themecolor icon-right-open-mini"></i>COMS
-                                     </li>
-                                          <li>
-                                              <i class="themecolor icon-right-open-mini"></i>OCTUPUS
-                                         </li>
-                                </ul>
-                            </div>
-                        </aside>
-                    </div>
-                    <div class="column one-fourth">
-                        <aside class="widget_text widget widget_custom_html">
-                            <a href="http://develop.imago.web.ve/eittech/develop1/html/content/robotics/service.html">
-                            <h4>Servicios</h4>
-                        </a>
-                            <div class="textwidget custom-html-widget">
-                                <ul>
-                                        <li>
-                                            <i class="themecolor icon-right-open-mini"></i> Consultoría
-                                        </li>
-                                    
-                                        <li>
-                                            <i class="themecolor icon-right-open-mini"></i> Diseño Web
-                                        </li>
-                                    
-                                        <li>
-                                            <i class="themecolor icon-right-open-mini"></i> E-Commerce
-                                        </li>
-                                   
-                                        <li>
-                                            <i class="themecolor icon-right-open-mini"></i> Marketing Web
-                                        </li> 
-                                </ul>
-                            </div>
-                        </aside>
-                    </div>
-                </div>
-            </div>-->
-            <div class="footer_copy">
-                <div class="container">
-                    <div class="column one">
-                        <a id="back_to_top" class="button button_js" href="#"><i class="icon-up-open-big"></i></a>
-                        <div class="copyright">
-                            &copy;  2019 Eittech <a target="_blank" rel="nofollow" href="#"></a>
-                        </div>
-                        <ul class="social"></ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <!-- Inclusión del footer -->
+		<?php 
+		if($config['config']['footer']['company']['active'] == 1){
+		  
+			include "footer.php";
+
+		} 
+		?>
     </div>
 
 
