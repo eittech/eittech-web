206a80c5ecd74bb85cfd654a9dd2b8226cc5ef41
José Solorzano <solorzano202009@gmail.com>
2019-07-03 16:33:10 -0400

    Traducción del footer y corrección de enlaces del home.

---
+++ lang/en/footer.php
@@ -0,0 +1,7 @@
+<?php
+
+// Textos de datos de menú
+
+$lang['footer']['textwidget'] = "We transform the way companies <br> work around the world.";
+
+?>
---
+++ lang/es/footer.php
@@ -0,0 +1,7 @@
+<?php
+
+// Textos de datos de menú
+
+$lang['footer']['textwidget'] = "Transformamos la forma de trabajar de <br> empresas en todo el mundo.";
+
+?>
